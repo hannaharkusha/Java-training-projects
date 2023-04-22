cf94c884e22596dede6f83c72c74b6020303495f
hannaharkusha <93228122+hannaharkusha@users.noreply.github.com>
2023-04-22 21:30:00 +0200

    Add files via upload

---
+++ AnagramChecker.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] s1Array = s1.toCharArray();
+        char[] s2Array = s2.toCharArray();
+
+        Arrays.sort(s1Array);
+        Arrays.sort(s2Array);
+
+        return Arrays.equals(s1Array, s2Array);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s1 = in.nextLine();
+        String s2 = in.nextLine();
+
+        System.out.println(areAnagrams(s1, s2));
+    }
+}
